147e65b5864e74f8864a56674ca8ea41525c9a27
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-06-01 22:12:33 -0500

    project initialized

---
+++ src/main/java/com/example/DemoRESTful/DemoResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoRESTful/DemoResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoRESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
